a10083069823d70c3a32b926aa6674feda3690d4
Una Kravets <unakravets@unakravets-macbookpro2.roam.corp.google.com>
2019-04-17 15:25:25 -0400

    adding more pages

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import {MDCTopAppBar} from '@material/top-app-bar/index';
+import {MDCTextField} from '@material/textfield';
+import {MDCTextFieldCharacterCounter} from '@material/textfield/character-counter';
+import {MDCRipple} from '@material/ripple';
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const nameField = new MDCTextField(document.querySelector('.mdc-text-field--name'));
+const emailField = new MDCTextField(document.querySelector('.mdc-text-field--email'));
+const messageField = new MDCTextField(document.querySelector('.mdc-text-field--message'));
+const characterCounter = new MDCTextFieldCharacterCounter(document.querySelector('.mdc-text-field-character-counter'));
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
